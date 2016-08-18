3810177b8552b95bdbc75ae23ba723ef420014e6
S. M. Catala <stephane@zenyway.com>
2016-08-18 10:20:04 +0200

    add implementations of DbIo and CoreDbIo

---
+++ src/core-db-io.ts
@@ -0,0 +1,173 @@
+/**
+ * Copyright 2016 Stephane M. Catala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * Limitations under the License.
+ */
+;
+import { __assign as assign } from 'tslib'
+import { WriteOpts, ReadOpts, DocRef, DocRevs, DocIdRange } from './'
+import { isObject, isFunction } from './utils'
+
+/**
+ * @public
+ * @factory CoreDbIoFactory
+ * @param {CoreDbIoSpec} spec
+ * @return {CoreDbIo}
+ */
+export interface CoreDbIoFactory {
+  (spec: CoreDbIoSpec): CoreDbIo
+}
+
+/**
+ * @public
+ * @interface CoreDbIoSpec
+ */
+export interface CoreDbIoSpec {
+  /**
+   * @prop {'write'|'read'} type of the CoreDbIo instance
+   */
+  type: 'write'|'read'
+  /**
+   * @prop {WriteOpts|ReadOpts} opts
+   */
+  opts: WriteOpts|ReadOpts
+}
+
+/**
+ * @public
+ * @interface CoreDbIo
+ * a pair of factories that wrap a {PouchDb} instance
+ * and return corresponding unit or bulk IO access methods
+ * which return a Promise of the result of the IO access operation.
+ * both factories return an IO access method of the same access type:
+ * write or read.
+ */
+export interface CoreDbIo {
+  /**
+   * @public
+   * @factory {(db: any): (src: DocRef) => Promise<DocRef>} unit
+   * wrap a {PouchDb} instance and return an IO access method
+   * that returns a Promise of the result of the IO access operation.
+   * @param {PouchDb} db
+   * @return {(src: DocRef) => Promise<DocRef>}
+   */
+  unit (db: any): (src: DocRef|DocRevs) => Promise<DocRef[]|DocRef>
+  /**
+   * @public
+   * @factory
+   */
+  bulk (db: any): (src: DocRef[]|DocIdRange) => Promise<DocRef[]>
+}
+
+export interface CoreDbIoMethod {
+  (src: DocRef[]|DocIdRange|DocRevs|DocRef): Promise<DocRef[]|DocRef>
+}
+
+/**
+ * @private
+ * @function isPouchDbLike
+ * duck-type checking
+ * @prop {any} val
+ * @return {boolean} true if val looks like
+ * a PouchDb instance for the purpose of this module,
+ * ie. it includes all the required methods
+ */
+export function isPouchDbLike (val: any): boolean {
+  return isObject(val)
+  && [ 'get', 'put', 'allDocs', 'bulkDocs' ]
+  .every(key => isFunction(val[key]))
+}
+
+/**
+ * @private
+ */
+abstract class CoreDbIoClass implements CoreDbIo {
+  static get types () {
+    return {
+      write: CoreDbWriteClass,
+      read: CoreDbReadClass
+    }
+  }
+  static newInstance = <CoreDbIoFactory> function (spec: CoreDbIoSpec): CoreDbIo {
+    return new CoreDbIoClass.types[spec.type](spec.opts)
+  }
+
+  abstract unit (db: any): (src: DocRef|DocRevs) => Promise<DocRef[]|DocRef>
+
+  abstract bulk (db: any): (src: DocRef[]|DocIdRange) => Promise<DocRef[]>
+}
+
+class CoreDbWriteClass extends CoreDbIoClass {
+  unit (db: any): (doc: DocRef) => Promise<DocRef> {
+    return (doc: DocRef) => db.put(doc, this.spec)
+      .then(toDocRef)
+  }
+
+  bulk (db: any): (docs: DocRef[]) => Promise<DocRef[]> {
+    return (docs: DocRef[]) => db.bulkDocs(docs, this.spec)
+      .then(toDocRefs)
+  }
+
+  constructor (private spec: WriteOpts) {
+    super()
+  }
+}
+
+class CoreDbReadClass extends CoreDbIoClass {
+  unit (db:any): (ref: DocRef|DocRevs) => Promise<DocRef[]|DocRef> {
+    return ref => db.get(ref._id, assign(unitOptsFrom(ref), this.spec))
+      .then(docsFromRevs)
+  }
+
+  bulk (db: any): (refs: DocRef[]|DocIdRange) => Promise<DocRef[]> {
+    return refs => db.allDocs(assign({}, bulkOptsFrom(refs), this.spec))
+      .then((res: AllDocsResult) => res.rows.map(row => row.doc))
+  }
+
+  constructor (private spec: ReadOpts) {
+    super()
+  }
+}
+
+function toDocRefs (res: any[]): DocRef[] {
+  return res.map(toDocRef)
+}
+
+function toDocRef (res: any): DocRef {
+  return res.ok ? { _id: res.id, _rev: res.rev } : res
+}
+
+function unitOptsFrom (ref: DocRef|DocRevs) {
+  return hasRevsArray(ref) ? { revs: toOptsRevs(ref._revs) } : { rev : ref._rev }
+}
+
+function hasRevsArray <D extends { _revs: string[] }> (val: any): val is D {
+  return Array.isArray(val._revs)
+}
+
+function toOptsRevs (revs: string[]): string[]|string {
+  return !revs.length ? 'all' : revs.slice()
+}
+
+function docsFromRevs (res: { ok: DocRef }[] | DocRef): DocRef[] | DocRef {
+  return Array.isArray(res) ? res.map(res => res.ok) : res
+}
+
+function bulkOptsFrom (refs: DocRef[]|DocIdRange) {
+  return Array.isArray(refs) ? { keys : refs.map(ref => ref._id) } : refs
+}
+
+interface AllDocsResult {
+  rows: { doc: DocRef }[]
+}
+
+const newCoreDbIo = CoreDbIoClass.newInstance
+export default newCoreDbIo
---
+++ src/db-io.ts
@@ -0,0 +1,213 @@
+/**
+ * Copyright 2016 Stephane M. Catala
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * Limitations under the License.
+ */
+;
+import { Observable } from '@reactivex/rxjs'
+import Promise = require('bluebird')
+import { WriteOpts, ReadOpts, DocRef, DocIdRange, DocRevs, DocId } from './'
+import newCoreDbIo, { CoreDbIo, CoreDbIoMethod, isPouchDbLike } from './core-db-io'
+import { logRx, isObject, isFunction, isString } from './utils'
+export { isPouchDbLike }
+
+/**
+ * @public
+ * @factory DbIoFactory
+ * @param {DbIoFactorySpec} spec
+ * @return {DbIo}
+ */
+export interface DbIoFactory {
+  (spec: DbIoFactorySpec): DbIo
+}
+
+/**
+ * @public
+ * @interface DbIoFactorySpec
+ */
+export interface DbIoFactorySpec {
+  /**
+   * @public
+   * @prop {WriteOpts} write
+   */
+  write: WriteOpts,
+  /**
+   * @public
+   * @prop {ReadOpts} read
+   */
+  read: ReadOpts,
+  /**
+   * @public
+   * @prop {{ write: CoreDbIo, read: CoreDbIo }} coreDbIo
+   */
+  coreDbIo?: {
+    write?: CoreDbIo
+    read?: CoreDbIo
+  }
+}
+
+/**
+ * @public
+ * @interface DbIo
+ * a pair of factories that wrap a {PouchDb} instance
+ * and return corresponding IO methods which
+ * return an Observable, which emits the result
+ * of the IO transaction.
+ */
+export interface DbIo {
+  /**
+   * @public
+   * @factory {DbIoFactoryMethod} write
+   * wrap a {PouchDb} instance and return an IO method
+   * for storing input documents to the database.
+   * the result of each storage operation is wrapped in a new Observable.
+   * @param {PouchDb} db
+   * @return {(src: DocRef[]|DocRef) => Observable<DocRef[]|DocRef>}
+   */
+  write (db: any): (src: DocRef[]|DocRef) => Observable<DocRef[]|DocRef>
+  /**
+   * @public
+   * @factory {DbIoFactoryMethod} read
+   * wrap a {PouchDb} instance and return an IO method
+   * for reading documents from the database.
+   * the result of each reading operation is wrapped in a new Observable.
+   * @param {PouchDb} db
+   * @return {(src: DocRef[]|DocIdRange|DocRevs|DocRef) => Observable<DocRef[]|DocRef>}
+   */
+  read (db: any):
+  (src: DocRef[]|DocIdRange|DocRevs|DocRef) => Observable<DocRef[]|DocRef>
+}
+
+/**
+ * @public
+ * @function DbIoDuckTypable
+ * duck-type validation: checks for `write` and `read` methods,
+ * and that both expect one argument.
+ * @prop {any} val?
+ * @return {val is DbIo}
+ */
+export interface DbIoDuckTypable {
+  (val?: any): val is DbIo
+}
+
+/**
+ * @private
+ * @class DbIoClass
+ * @implements {DbIo}
+ */
+class DbIoClass implements DbIo {
+  /**
+   * @public
+   * @see {DbIoFactory}
+   */
+  static newInstance: DbIoFactory =
+  function (spec: DbIoFactorySpec): DbIo {
+    const coreDbIo = {
+      write: spec.coreDbIo && spec.coreDbIo.write ||
+        newCoreDbIo({ type: 'write' , opts: spec.write }),
+      read: spec.coreDbIo && spec.coreDbIo.read ||
+        newCoreDbIo({ type: 'read' , opts: spec.read })
+    }
+    return new DbIoClass(coreDbIo)
+  }
+  /**
+   * @public
+   * @see {DbIoDuckTypable}
+   */
+  static isDbIo: DbIoDuckTypable =
+  function (val?:any): val is DbIo {
+    return isObject(val) && isFunction(val.write) && isFunction(val.read)
+  }
+  /**
+   * @public
+   * @see {DbIo#write}
+   */
+  write: (db: any) => (src: DocRef[]|DocRef) => Observable<DocRef[]|DocRef>
+  /**
+   * @public
+   * @see {DbIo#read}
+   */
+  read: (db: any) =>
+  (src: DocRef[]|DocIdRange|DocRevs|DocRef) => Observable<DocRef[]|DocRef>
+  /**
+   * @private
+   * @constructor
+   * @param {{ write: CoreDbIo, read: CoreDbIo }} coreDbIo
+   */
+  constructor (private coreDbIo: { write: CoreDbIo, read: CoreDbIo }) {}
+}
+
+DbIoClass.prototype.write = createDbIoMethod('write')
+DbIoClass.prototype.read = createDbIoMethod('read')
+
+/**
+ * @private
+ * @function createDbIoMethod
+ * @prop {'write'|'read'} ioKey type of IO method to generate
+ * @return {DbIoFactoryMethod}
+ */
+function createDbIoMethod (ioKey: 'write'|'read'): DbIoFactoryMethod {
+  return function (db: any) {
+    return (src: DocRef[]|DocIdRange|DocRevs|DocRef) => {
+      const coreDbIoKey = coreDbIoKeyFor(src)
+      const coreDbIoMethod: CoreDbIoMethod =
+      this.coreDbIo[ioKey][coreDbIoKey](db)
+
+      return Observable.fromPromise(Promise.try(() => coreDbIoMethod(src)))
+      .do(logRx(`rx-pouchdb:${ioKey}:${coreDbIoKey}`))
+    }
+  }
+}
+
+/**
+ * @private
+ * @function coreDbIoKeyFor
+ * @param {DocRef[]|DocIdRange|DocRevs|DocRef} src
+ * @return {'bulk'|'unit'}
+ */
+function coreDbIoKeyFor (src: DocRef[]|DocIdRange|DocRevs|DocRef): 'bulk'|'unit' {
+  return !isString((<any>src)._id) ? 'bulk' : 'unit'
+}
+
+/**
+ * @private
+ * @factory DbIoFactoryMethod
+ * wrap a {PouchDb} instance and return an IO method
+ * for storing/reading documents to/from the database.
+ * the result of each IO operation is wrapped in a new Observable.
+ */
+interface DbIoFactoryMethod {
+  (db: any): (src: DocRef[]|DocIdRange|DocRevs|DocRef) => Observable<DocRef[]|DocRef>
+}
+
+/**
+ * @private
+ * @function isDbIoFactoryMethod
+ * duck-type validation
+ * @param {any} val
+ * @return {val is DbIoFactoryMethod}
+ */
+function isDbIoFactoryMethod (val: any): val is DbIoFactoryMethod {
+  return isFunction(val) && (val.length === 1)
+}
+
+/**
+ * @public
+ * @see {DbIoDuckTypable}
+ */
+export const isDbIoLike = DbIoClass.isDbIo
+
+/**
+ * @public
+ * @see {DbIoFactory}
+ */
+const newDbIo: DbIoFactory = DbIoClass.newInstance
+export default newDbIo
